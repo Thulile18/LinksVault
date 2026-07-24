7311d696f92b929e5913853168525c7f0a1cb20d
Thulile18 <busisiwegqada0@gmail.com>
2026-07-24 10:30:17 +0200

    Update header title formatting
    Added spaces around the title for better formatting.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="header">
-      <h1>MaNcane's Links Vault</h1>
+      <h1> MaNcane's Links Vault </h1>
       <p>Save and organize your favorite links</p>
     </header>
   );
