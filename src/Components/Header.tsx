310e11f4dfd3ceceba23a17bb7a37a3a9809d64a
Thulile18 <busisiwegqada0@gmail.com>
2026-07-24 10:30:38 +0200

    Fix whitespace in paragraph element

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <h1> MaNcane's Links Vault </h1>
-      <p>Save and organize your favorite links</p>
+      <p> Save and organize your favorite links </p>
     </header>
   );
 }
