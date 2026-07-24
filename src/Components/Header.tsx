d2bf001f03917fb40d4ca24e3b822a6e9c6130d1
Thulile18 <busisiwegqada0@gmail.com>
2026-07-24 10:29:36 +0200

    Update Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+export default function Header() {
   return (
     <header className="header">
-      <h1> MaNcane's Links Vault </h1>
-      <p> Save and organize your favorite links </p>
+      <h1>MaNcane's Links Vault</h1>
+      <p>Save and organize your favorite links</p>
     </header>
   );
-};
-
-export default Header;
+}
